6b351a504d1fb64faef9cf6f694d6031584a2fae
Anton Iutin <anton.iutin@utair.ru>
2026-07-25 01:27:16 +0300

    Fix: use BASE_URL for sprite paths + try/catch fallback

--- assets/BufferResource-DETPjhhI.js
+++ assets/BufferResource-DETPjhhI.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-C3GLtQe9.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-DZMwDDDI.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
